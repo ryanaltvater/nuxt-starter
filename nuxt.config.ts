1e352f8b7087488006dd79fda85ee9cd05359115
Ryan Altvater <ryan@ryanaltvater.com>
2023-06-15 00:05:54 -0400

    Added site-specific meta type and image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,15 +8,17 @@ export default defineNuxtConfig({
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
                 { name: 'author', content: 'Site author' },
                 { name: 'copyright', content: 'Site copyright' },
-                { property: 'og:locale', content: 'en_US' }
+                { property: 'og:locale', content: 'en_US' },
+                { property: 'og:type', content: 'website' },
+                { property: 'og:image', content: '' }
             ],
             link: [
                 { rel: 'icon', href: 'favicon.ico' },
                 // { rel: 'stylesheet', href: '', crossorigin: '' }
             ],
             script: [
-                { src: 'https://code.jquery.com/jquery-3.6.0.min.js', tagPosition: 'head' },
-                // { src: '/assets/js/scripts.js', tagPosition: 'bodyClose' }
+                // { src: 'https://code.jquery.com/jquery-3.6.0.min.js', tagPosition: 'head' },
+                { src: 'assets/js/scripts.js', tagPosition: 'bodyClose' }
             ],
             bodyAttrs: { class: '' }
         }
