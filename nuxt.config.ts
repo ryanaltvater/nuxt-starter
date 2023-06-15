481383ccbca1c66bcd6c999b086af19454ccfca5
Ryan Altvater <ryan@ryanaltvater.com>
2023-06-15 00:12:02 -0400

    Updated titleTemplate format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     app: {
         head: {
-            titleTemplate: '%s - Site title',
+            titleTemplate: '%s · Nuxt Starter',
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
